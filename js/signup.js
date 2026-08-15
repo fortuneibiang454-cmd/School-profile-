8583fa867e9b30a5b5218b139b0a1913d57c4623
fortuneibiang454-cmd <fortuneibiang454@gmail.com>
2026-08-15 19:21:27 +0100

    Create signup.js

---
+++ js/signup.js
@@ -0,0 +1,30 @@
+hereimport { auth } from "./firebase.js";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    console.log("Account created:", userCredential.user.uid);
+
+    alert("Account created successfully!");
+
+    window.location.href = "index.html";
+
+  } catch (error) {
+    console.error(error);
+
+    alert(error.message);
+  }
+});
