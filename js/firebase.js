d78687e13855df73b2d7b0ad7390ef027f974577
fortuneibiang454-cmd <fortuneibiang454@gmail.com>
2026-08-15 18:51:10 +0100

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+hereimport { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "school-connect-11578.firebaseapp.com",
+  projectId: "school-connect-11578",
+  storageBucket: "school-connect-11578.firebasestorage.app",
+  messagingSenderId: "85015597520",
+  appId: "1:85015597520:web:6d1c895eade85026c4e9d5",
+  measurementId: "G-XQTW122JZ3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
