0c9747616fc593428ef923678527c328f9c09f8d
fortuneibiang454-cmd <fortuneibiang454@gmail.com>
2026-08-16 09:08:06 +0100

    Update Firebase imports and configuration

--- js/firebase.js
+++ js/firebase.js
@@ -1,53 +1,46 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "school-connect-11578.firebaseapp.com",
-  projectId: "school-connect-11578",
-  storageBucket: "school-connect-11578.firebasestorage.app",
-  messagingSenderId: "85015597520",
-  appId: "1:85015597520:web:6d1c895eade85026c4e9d5",
-  measurementId: "G-XQTW122JZ3"
+
+  apiKey: "PASTE_YOUR_REAL_API_KEY_HERE",
+
+  authDomain:
+    "school-connect-11578.firebaseapp.com",
+
+  projectId:
+    "school-connect-11578",
+
+  storageBucket:
+    "school-connect-11578.firebasestorage.app",
+
+  messagingSenderId:
+    "85015597520",
+
+  appId:
+    "1:85015597520:web:6d1c895eade85026c4e9d5",
+
+  measurementId:
+    "G-XQTW122JZ3"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+const app =
+  initializeApp(firebaseConfig);
 
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "school-connect-11578.firebaseapp.com",
-  projectId: "school-connect-11578",
-  storageBucket: "school-connect-11578.firebasestorage.app",
-  messagingSenderId: "85015597520",
-  appId: "1:85015597520:web:6d1c895eade85026c4e9d5",
-  measurementId: "G-XQTW122JZ3"
-};
+export const auth =
+  getAuth(app);
 
-const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "school-connect-11578.firebaseapp.com",
-  projectId: "school-connect-11578",
-  storageBucket: "school-connect-11578.firebasestorage.app",
-  messagingSenderId: "85015597520",
-  appId: "1:85015597520:web:6d1c895eade85026c4e9d5",
-  measurementId: "G-XQTW122JZ3"
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db =
+  getFirestore(app);
