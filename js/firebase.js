c72164e762fcde85ff0266c0fc9583e6b33c27bb
fortuneibiang454-cmd <fortuneibiang454@gmail.com>
2026-08-16 09:52:38 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -10,37 +10,19 @@ import {
 
 
 const firebaseConfig = {
-
-  apiKey: "PASTE_YOUR_REAL_API_KEY_HERE",
-
-  authDomain:
-    "school-connect-11578.firebaseapp.com",
-
-  projectId:
-    "school-connect-11578",
-
-  storageBucket:
-    "school-connect-11578.firebasestorage.app",
-
-  messagingSenderId:
-    "85015597520",
-
-  appId:
-    "1:85015597520:web:6d1c895eade85026c4e9d5",
-
-  measurementId:
-    "G-XQTW122JZ3"
-
+  apiKey: "AIzaSyBeBJ2fzqhq_yUMUxyq-OHMgI5eBG9t9po",
+  authDomain: "school-connect-11578.firebaseapp.com",
+  databaseURL: "https://school-connect-11578-default-rtdb.firebaseio.com",
+  projectId: "school-connect-11578",
+  storageBucket: "school-connect-11578.firebasestorage.app",
+  messagingSenderId: "85015597520",
+  appId: "1:85015597520:web:6d1c895eade85026c4e9d5",
+  measurementId: "G-XQTW122JZ3"
 };
 
 
-const app =
-  initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
+export const auth = getAuth(app);
 
-export const auth =
-  getAuth(app);
-
-
-export const db =
-  getFirestore(app);
+export const db = getFirestore(app);
